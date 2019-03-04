440052a61b986bdce2419774cffa146792fde2f9
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-03-04 11:29:53 -0500

    New about page and more content

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: 'Bennett Garner: Python Full Stack Developer' 
+  res.render('index', {
+    title: 'Bennett Garner: Python Full Stack Developer'
+  });
+});
+
+/* GET home page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', {
+    title: 'About Bennett Garner: Python Full Stack Developer'
   });
 });
 
