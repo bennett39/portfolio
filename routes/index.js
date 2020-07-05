6b12b3d37422762571ffe8e1cfd3938b2dbadc8d
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2020-07-05 17:34:40 -0400

    New pages, books

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Bennett Garner: Python Full Stack Developer'
   });
 });
 
-/* GET about page. */
 router.get('/about', function(req, res, next) {
   res.render('about', {
     title: 'About Bennett Garner: Python Full Stack Developer'
+  });
+});
+
+router.get('/books', function(req, res, next) {
+  res.render('books', {
+    title: 'Books by Bennett Garner'
+  });
+});
+
+router.get('/solutions', function(req, res, next) {
+  res.render('solutions', {
+      title: 'SOLUTIONS: Use Code to Solve Real Problems by Bennett Garner'
+  });
+});
+
+router.get('/buy_solutions', function(req, res, next) {
+  res.render('buy_solutions', {
+      title: 'SOLUTIONS: Use Code to Solve Real Problems by Bennett Garner'
   });
 });
 
